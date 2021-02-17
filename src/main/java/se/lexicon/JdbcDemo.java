191e219b7dd8dd07a0b4627861c8188cc05ac58c
mehrdad-javan <mehrdad.javan@adapta.se>
2021-02-17 09:23:16 +0100

    add mysql dependency and define mysql connection

--- src/main/java/se/lexicon/JdbcDemo.java
+++ src/main/java/se/lexicon/JdbcDemo.java
@@ -1,10 +1,21 @@
 package se.lexicon;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcDemo {
 
     public static void main(String[] args) {
 
+        // define my sql connection
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/world","root","root");
 
+            System.out.println("Connection is established");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
