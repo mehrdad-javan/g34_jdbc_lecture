8a2411322ae81ce01e1dd9197fee82f9e745d797
mehrdad-javan <mehrdad.javan@adapta.se>
2021-02-17 09:27:55 +0100

    create statement and define select query

--- src/main/java/se/lexicon/JdbcDemo.java
+++ src/main/java/se/lexicon/JdbcDemo.java
@@ -3,6 +3,7 @@ package se.lexicon;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcDemo {
 
@@ -11,7 +12,11 @@ public class JdbcDemo {
         // define my sql connection
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/world","root","root");
-
+            // define statement
+            Statement statement= connection.createStatement();
+            String selectAllCities= "select * from city";
+            // execute select query
+            statement.executeQuery(selectAllCities);
             System.out.println("Connection is established");
         } catch (SQLException e) {
             e.printStackTrace();
