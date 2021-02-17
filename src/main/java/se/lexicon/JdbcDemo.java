1b9a942127193350a2b98d428fa7b3789612bcc9
mehrdad-javan <mehrdad.javan@adapta.se>
2021-02-17 09:36:39 +0100

    define result set and get data from result set

--- src/main/java/se/lexicon/JdbcDemo.java
+++ src/main/java/se/lexicon/JdbcDemo.java
@@ -1,9 +1,6 @@
 package se.lexicon;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JdbcDemo {
 
@@ -11,12 +8,23 @@ public class JdbcDemo {
 
         // define my sql connection
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/world","root","root");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "root");
             // define statement
-            Statement statement= connection.createStatement();
-            String selectAllCities= "select * from city";
+            Statement statement = connection.createStatement();
+            String selectAllCities = "select id,name,population from city";
             // execute select query
-            statement.executeQuery(selectAllCities);
+            ResultSet resultSet = statement.executeQuery(selectAllCities);
+            // get data using resultSet
+            while (resultSet.next()) {
+                /*
+                    int id = resultSet.getInt(1);
+                    String name = resultSet.getString(2);
+                 */
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String population = resultSet.getString("population");
+                System.out.println("Id: " + id + " Name: " + name + " Population: " + population);
+            }
             System.out.println("Connection is established");
         } catch (SQLException e) {
             e.printStackTrace();
